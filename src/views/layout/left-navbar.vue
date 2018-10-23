75acafe9ae56201818ad924883ba007c5e1dca83
shenlianxi <shenlianxi@ytdinfo.com>
2018-10-23 17:42:46 +0800

    导航条

--- src/views/layout/left-navbar.vue
+++ src/views/layout/left-navbar.vue
@@ -3,37 +3,52 @@
     <el-menu class="el-menu-vertical-demo"
       @open="handleOpen"
       @close="handleClose"
-      :collapse="isCollapse">
+      :collapse="isCollapse"
+      default-active="1-1">
       <el-submenu index="1">
         <template slot="title">
           <i class="el-icon-location"></i>
-          <span slot="title">导航一</span>
+          <span slot="title">系统信息</span>
         </template>
         <el-menu-item-group>
-          <span slot="title">分组一</span>
-          <el-menu-item index="1-1">选项1</el-menu-item>
-          <el-menu-item index="1-2">选项2</el-menu-item>
+          <span slot="title">使用指南</span>
+          <el-menu-item index="1-1">如何创建数据</el-menu-item>
+          <el-menu-item index="1-2">如何管理数据</el-menu-item>
         </el-menu-item-group>
-        <el-menu-item-group title="分组2">
-          <el-menu-item index="1-3">选项3</el-menu-item>
+        <el-menu-item-group title="账号">
+          <el-menu-item index="1-3">账户信息维护</el-menu-item>
+          <el-menu-item index="1-4">上传头像</el-menu-item>
         </el-menu-item-group>
-        <el-submenu index="1-4">
-          <span slot="title">选项4</span>
+        <!-- <el-submenu index="1-4">
+          <span slot="title">协作部门</span>
           <el-menu-item index="1-4-1">选项1</el-menu-item>
-        </el-submenu>
+        </el-submenu> -->
       </el-submenu>
       <el-menu-item index="2">
         <i class="el-icon-menu"></i>
-        <span slot="title">导航二</span>
+        <span slot="title">推广中心</span>
+        <!-- 广告推广出租等信息 -->
       </el-menu-item>
-      <el-menu-item index="3" disabled>
+      <el-menu-item index="3">
         <i class="el-icon-document"></i>
-        <span slot="title">导航三</span>
+        <span slot="title">生活指南</span>
       </el-menu-item>
-      <el-menu-item index="4">
+      <!-- <el-menu-item index="4">
         <i class="el-icon-setting"></i>
         <span slot="title">导航四</span>
       </el-menu-item>
+      <el-menu-item index="5">
+        <i class="el-icon-menu"></i>
+        <span slot="title">导航二</span>
+      </el-menu-item>
+      <el-menu-item index="6" disabled>
+        <i class="el-icon-document"></i>
+        <span slot="title">导航三</span>
+      </el-menu-item>
+      <el-menu-item index="7">
+        <i class="el-icon-setting"></i>
+        <span slot="title">导航四</span>
+      </el-menu-item> -->
     </el-menu>
   </div>
 </template>
@@ -42,7 +57,7 @@
 export default {
   data() {
     return {
-      isCollapse: true
+      isCollapse: false
     };
   },
   props: {
--- src/views/layout/navbar.vue
+++ src/views/layout/navbar.vue
@@ -1,30 +1,36 @@
 <template>
   <div class="top-nav">
     <el-menu class="navbar"
-      :default-active="activeIndex"
       mode="horizontal"
       @select="handleSelect"
       background-color="#545c64"
       text-color="#fff"
       active-text-color="#ffd04b">
       <!-- 导航栏需要的功能再次拓展 -->
-      <div class="nav-pull-push" @click="handleChangeMenuType">
+      <div v-if="leftExsistState" class="nav-pull-push" @click="handleChangeMenuType">
         <icon-svg :icon-class="leftMenuType" ></icon-svg>
       </div>
-      <el-menu-item index="1">处理中心</el-menu-item>
+      <div class="profile-photo">
+        <img style="height:100%; width:100%" src="@/assets/image/gears.png">;
+      </div>
+      <el-menu-item index="1">数据中心</el-menu-item>
       <el-submenu index="2">
-        <template slot="title">我的工作台</template>
-        <el-menu-item index="2-1">选项1</el-menu-item>
-        <el-menu-item index="2-2">选项2</el-menu-item>
-        <el-menu-item index="2-3">选项3</el-menu-item>
+        <template slot="title">维护驾驶舱</template>
+        <el-menu-item index="2-1">维护列表</el-menu-item>
+        <!-- 显示维护的医护人员列表 -->
+        <el-menu-item index="2-2">药品信息维护</el-menu-item>
+        <!-- 提供修改价格等信息, 查询等功能, 类别修改 -->
+        <el-menu-item index="2-3">顾客信息维护</el-menu-item>
       </el-submenu>
-        <!-- <el-submenu index="2-4">
-          <template slot="title">选项4</template>
-          <el-menu-item index="2-4-1">选项1</el-menu-item>
-          <el-menu-item index="2-4-2">选项2</el-menu-item>
-          <el-menu-item index="2-4-3">选项3</el-menu-item>
-        </el-submenu> -->
-      <el-menu-item index="3" disabled>消息中心</el-menu-item>
+        <el-submenu index="2-4">
+          <template slot="title">库存管理</template>
+          <el-menu-item index="2-4-1">药品入库</el-menu-item>
+          <el-menu-item index="2-4-2">药品出库</el-menu-item>
+        </el-submenu>
+        <el-menu-item index="2-5">订单管理</el-menu-item>
+        <el-menu-item index="3">分析系统</el-menu-item>
+      <el-menu-item index="4">报表系统</el-menu-item>
+      <el-menu-item index="5">销售系统</el-menu-item>
     </el-menu>
     <left-nav v-if="leftExsistState" :openLeftState="leftMenuType"></left-nav>
   </div>
@@ -35,7 +41,7 @@ import leftNav from './left-navbar';
 export default {
   data() {
     return {
-      activeIndex: 1,
+      activeIndex: '1',
       leftMenuType: 'zhankaicaidan'
     };
   },
@@ -77,5 +83,11 @@ export default {
   margin-left: 10px;
   font-size: 30px;
   color: #409EFF;
+}
+.profile-photo {
+  float: left;
+  height: 40px;
+  margin: 10px 10px 10px 20px;
+  width: 40px;
 }
 </style>
